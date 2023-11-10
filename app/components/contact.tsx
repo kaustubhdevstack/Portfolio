66b7489e354bdb73ca655f4dae557f81a33636a2
Kaustubh Shinde <143103243+kaustubhdevstack@users.noreply.github.com>
2023-11-10 18:09:15 +0530

    Update contact.tsx

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -7,41 +7,47 @@ import Submit from './submit';
 
 export default function Contact() {
   const ref = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = async (formData) => {
+    try {
+      const response = await sendEmail(formData);
+
+      // Check if the response has the expected structure
+      if (!response || !response.data) {
+        throw new Error("Invalid response structure");
+      }
+
+      // Destructure the data from the response
+      const { data, error } = response;
+
+      // Check if there's an error in the response
+      if (error) {
+        toast.error(error);
+        return;
+      }
+
+      toast.success("Email sent successfully!");
+      ref.current?.reset();
+    } catch (error) {
+      console.error("Error sending email:", error);
+      toast.error("Failed to send email. Please try again.");
+    }
+  };
+
   return (
-        <section className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center">
-          <h2 className="text-3xl font-medium capitalize mb-1 text-center">Get in touch</h2>
-          <p className="text-center 2x text-gray-800 mb-8">Want my help for a project? then just reach out to me!</p>
-
-          <form 
-          ref={ref}
-          className="mt-10 flex flex-col h-screen" 
-          action={async (formData) => {
-            const { data, error } = await sendEmail(formData);
-  
-            if (error) {
-              toast.error(error);
-              return;
-            }
-  
-            toast.success("Email sent successfully!");
-            ref.current?.reset();
-          }}
-          >
-            <input 
-            className="h-10 rounded-lg border-1 p-4" 
-            name="senderEmail"
-            type="email" 
-            placeholder="Enter your email" 
-            required maxLength={700} />
-
-            <textarea 
-            className="h-52 mt-2 rounded-lg border-1 p-4" 
-            name="message"
-            placeholder="Type something..." 
-            required maxLength={700}/>
-
-            <Submit/>
-          </form>
-        </section>
-  )
+    <section className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center">
+      {/* ... (other JSX) */}
+      <form
+        ref={ref}
+        className="mt-10 flex flex-col h-screen"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(new FormData(e.target));
+        }}
+      >
+        {/* ... (other input and textarea elements) */}
+        <Submit />
+      </form>
+    </section>
+  );
 }
