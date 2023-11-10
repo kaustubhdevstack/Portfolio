6d4406e66b299e505db3087c287b0002191104da
Kaustubh Shinde <143103243+kaustubhdevstack@users.noreply.github.com>
2023-11-10 18:11:22 +0530

    Update contact.tsx

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,6 +1,5 @@
-'use client'
-import React from 'react';
-import { useRef } from 'react';
+// Import statements
+import React, { useRef, FormEvent } from 'react';
 import { sendEmail } from '@/actions/sendEmail';
 import { toast } from 'react-hot-toast';
 import Submit from './submit';
@@ -8,7 +7,7 @@ import Submit from './submit';
 export default function Contact() {
   const ref = useRef<HTMLFormElement>(null);
 
-  const handleSubmit = async (formData) => {
+  const handleSubmit = async (formData: FormData) => {
     try {
       const response = await sendEmail(formData);
 
@@ -36,16 +35,35 @@ export default function Contact() {
 
   return (
     <section className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center">
-      {/* ... (other JSX) */}
+      <h2 className="text-3xl font-medium capitalize mb-1 text-center">Get in touch</h2>
+      <p className="text-center 2x text-gray-800 mb-8">Want my help for a project? then just reach out to me!</p>
+
       <form
         ref={ref}
         className="mt-10 flex flex-col h-screen"
-        onSubmit={(e) => {
+        onSubmit={(e: FormEvent) => {
           e.preventDefault();
-          handleSubmit(new FormData(e.target));
+          const formData = new FormData(e.target as HTMLFormElement);
+          handleSubmit(formData);
         }}
       >
-        {/* ... (other input and textarea elements) */}
+        <input
+          className="h-10 rounded-lg border-1 p-4"
+          name="senderEmail"
+          type="email"
+          placeholder="Enter your email"
+          required
+          maxLength={700}
+        />
+
+        <textarea
+          className="h-52 mt-2 rounded-lg border-1 p-4"
+          name="message"
+          placeholder="Type something..."
+          required
+          maxLength={700}
+        />
+
         <Submit />
       </form>
     </section>
