00fc8019427df5223909ae7d82ef85907b1db490
Kaustubh Shinde <143103243+kaustubhdevstack@users.noreply.github.com>
2023-11-10 17:49:27 +0530

    Update sendEmail.ts

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -7,39 +7,34 @@ import ContactTemplate from "@/app/email/ContactTemplate";
 const resend = new Resend(process.env.API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-        const senderEmail = formData.get('senderEmail');
-        const message = formData.get('message');
+    const senderEmail = formData.get('senderEmail');
+    const message = formData.get('message');
 
-        if(!validateString(senderEmail, 700)) {
-                return {
-                       error: 'Invalid Email'
-                };
-        }
-        if(!validateString(message, 700)) {
-                return {
-                       error: 'Invalid Message'
-                };
-        }
-
-        let data;
-        try {
-                data = await resend.emails.send({
-                        from: 'Kaustubh Contact Form <onboarding@resend.dev>',
-                        to: 'sonicai.me@gmail.com',
-                        subject: 'Message from Kaustubh Porfolio',
-                        reply_to: senderEmail as string,
-                        react: React.createElement(ContactTemplate, {
-                                message: message as string,
-                                senderEmail: senderEmail as string,
-                        }),
-                });
-        } catch (error) {
-              return {
-                error: getErrorMessage(error),
-              }  
-        }
-
+    if (!validateString(senderEmail, 700) || !validateString(message, 700)) {
         return {
-                data,
+            error: 'Invalid Email or Message',
         };
+    }
+
+    try {
+        const response = await resend.emails.send({
+            from: 'Kaustubh Contact Form <onboarding@resend.dev>',
+            to: 'sonicai.me@gmail.com',
+            subject: 'Message from Kaustubh Portfolio',
+            reply_to: senderEmail as string,
+            react: React.createElement(ContactTemplate, {
+                message: message as string,
+                senderEmail: senderEmail as string,
+            }),
+        });
+
+        // Assuming the email sending is successful
+        return {
+            success: 'Email sent successfully',
+        };
+    } catch (error) {
+        return {
+            error: getErrorMessage(error),
+        };
+    }
 };
